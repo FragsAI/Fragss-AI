30b53bf9af2153ec9e5cd67a6b6bb077f6105f66
Mohammed Amaan <122539252+123amaan@users.noreply.github.com>
2025-01-25 12:22:52 +0530

    Update voiceover.py

--- final/voiceover.py
+++ final/voiceover.py
@@ -1,10 +1,10 @@
 import requests
 
 # API key for Elevenlabs
-api_key = "sk_516b6416f820c0c196426bb27728e2ff31b95db18955b1c1"
-url = "https://api.elevenlabs.io/v1/text-to-speech"
+api_key = "sk_6f62bfcfbfc050e98fc97db2e8e8701db7a52654c95d788c"
+url = "https://api.elevenlabs.io/v1/text-to-speech/cgSgspJ2msm6clMCkdW9" # https://api.elevenlabs.io/v1/text-to-speech/Voiceid
 
-def generate_voiceover(text, voice="Bella", output_path="voiceover.mp3"):
+def generate_voiceover(text, voice="Jessica", output_path="voiceover.mp3"):
     """
     Generates a voiceover for the given text using Elevenlabs API.
     Args:
